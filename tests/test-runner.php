9d86789ca4a54c4701dd981470403aeb9b373b91
Nellutla L N Rao <nlnrao228@gmail.com>
2025-10-08 16:47:57 +0100

    Updated Projects

--- tests/test-runner.php
+++ tests/test-runner.php
@@ -1,4 +1,13 @@
 <?php
-require  __DIR__.'/utils.api.php';
-require  __DIR__.'/utils.api.php';
+require  __DIR__.'/utils/utils.api.php';
+require  __DIR__.'/utils/gen.report.php';
+
+$API_PREFIX = "http://localhost/aiwl-app2/";
+$API_COUNTRIES_LIST = [
+    "url" => "get/countries/list",
+    "method" => "GET"
+];
+
+$apiResponse = callApi($API_PREFIX.$API_COUNTRIES_LIST["url"], $API_COUNTRIES_LIST["method"]);
+
 ?>
